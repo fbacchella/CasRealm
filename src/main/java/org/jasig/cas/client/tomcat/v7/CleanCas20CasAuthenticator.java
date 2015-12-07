88ac3df71b283c9389e05910282f3c67af4dc7fd
Fabrice Bacchella <fbacchella@spamcop.net>
2015-12-07 16:02:44 +0100

    Adding a map to remove "ticket=.." in the query.

---
+++ src/main/java/org/jasig/cas/client/tomcat/v7/CleanCas20CasAuthenticator.java
@@ -0,0 +1,36 @@
+package org.jasig.cas.client.tomcat.v7;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import javax.servlet.ServletException;
+
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
+import org.apache.catalina.valves.ValveBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CleanCas20CasAuthenticator extends ValveBase {
+
+    private final static Logger logger = LoggerFactory.getLogger(CleanCas20CasAuthenticator.class);
+    Pattern filter = java.util.regex.Pattern.compile("ticket=[A-Za-z0-9\\.-]+");
+
+    @Override
+    public void invoke(Request request, Response response)
+            throws IOException, ServletException {
+        if(request.getParameter("ticket") != null) {
+            String query = request.getQueryString();
+            query = filter.matcher(query).replaceAll("");
+            if(query.length() > 0) {
+                query = "?" + query;
+            }
+            logger.debug("send redirect to {}{}", request.getRequestURI(), query);
+            response.sendRedirect(request.getRequestURI() + query);
+        } else {
+            getNext().invoke(request, response);
+        }
+
+    }
+
+}
