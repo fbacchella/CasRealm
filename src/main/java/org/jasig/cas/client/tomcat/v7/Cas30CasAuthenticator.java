7ab51a8764c2ad015824ebd1e9bcc023dc690a3b
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2018-10-02 20:03:48 +0200

    Adding support for protocol CAS 3.0.

---
+++ src/main/java/org/jasig/cas/client/tomcat/v7/Cas30CasAuthenticator.java
@@ -0,0 +1,38 @@
+package org.jasig.cas.client.tomcat.v7;
+
+import org.apache.catalina.LifecycleException;
+import org.jasig.cas.client.validation.Cas30ServiceTicketValidator;
+import org.jasig.cas.client.validation.TicketValidator;
+
+public class Cas30CasAuthenticator extends AbstractCasAuthenticator {
+
+    public static final String AUTH_METHOD = "CAS30";
+
+    private static final String NAME = Cas30CasAuthenticator.class.getName();
+
+    private Cas30ServiceTicketValidator ticketValidator;
+
+    protected TicketValidator getTicketValidator() {
+        return ticketValidator;
+    }
+
+    protected String getAuthenticationMethod() {
+        return AUTH_METHOD;
+    }
+
+    protected String getName() {
+        return NAME;
+    }
+
+    protected void startInternal() throws LifecycleException {
+        super.startInternal();
+        ticketValidator = new Cas30ServiceTicketValidator(getCasServerUrlPrefix());
+        if (getEncoding() != null) {
+            ticketValidator.setEncoding(getEncoding());
+        }
+        ticketValidator.setProxyCallbackUrl(getProxyCallbackUrl());
+        ticketValidator.setProxyGrantingTicketStorage(ProxyCallbackValve.getProxyGrantingTicketStorage());
+        ticketValidator.setRenew(isRenew());
+    }
+
+}
